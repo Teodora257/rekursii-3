0d5c20b9aa83a9dc8027ab44221785fb8cf8006c
MEGA <MEGA@gmail.com>
2025-02-14 18:20:16 +0200

    Add project files.

---
+++ rekursii-3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace rekursii_3
+{
+    using System;
+
+    class Program
+    {
+        static int ReverseNumber(int n, int reversed = 0)
+        {         
+            if (n == 0)
+            {
+                return reversed;
+            }         
+            return ReverseNumber(n/10, reversed * 10 + n% 10);
+        }
+
+        static void Main(string[] args)
+        {             
+            Console.WriteLine("Vyvedete chislo: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int reversedNumber = ReverseNumber(number);
+            Console.WriteLine ($"Obyrnatoto chislo e: {reversedNumber}");
+        }
+    }
+}
